74f800ec5f5607208d921dc0852356fcd6ed57a6
Evandro oliveira <vandoaparecido@hotmail.com>
2022-06-07 21:46:25 -0300

    :money_with_wings: Criando estilos globais

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { Dashboard } from "./src/screens/Dashboard";
 
+import theme from "./src/global/styles/theme";
+import { ThemeProvider } from "styled-components";
+
 export default function App() {
-  return <Dashboard />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
